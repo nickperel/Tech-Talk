f9d6def8eeda54c8b720cc6d52b0fafbaa5f8977
Kakashi <nickperel@live.com>
2022-02-06 21:14:17 -0500

    created the functionality to be able to edit a post's content

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -90,7 +90,8 @@ router.post('/', withAuth, (req, res) => {
 router.put('/:id', withAuth, (req, res) => {
   Post.update(
     {
-      title: req.body.title
+      title: req.body.title,
+      blog_content: req.body.postContent
     },
     {
       where: {
--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -1,14 +1,16 @@
 async function editFormHandler(event) {
   event.preventDefault();
 
-  const title = document.querySelector('input[name="post-title"]').value.trim();
+  const postContent = document.querySelector('#post-content').value.trim();
+  const title = document.querySelector('#title').value.trim();
   const id = window.location.toString().split('/')[
     window.location.toString().split('/').length - 1
   ];
   const response = await fetch(`/api/posts/${id}`, {
     method: 'PUT',
     body: JSON.stringify({
-      title
+      title,
+      postContent
     }),
     headers: {
       'Content-Type': 'application/json'
