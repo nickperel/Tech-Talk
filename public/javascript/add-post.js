443ce380cd7a5cd2225fdb8c63076154f7abca69
Kakashi <nickperel@live.com>
2022-02-06 20:59:03 -0500

    made adjustments to how creating a new post is seen from the UI

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -2,7 +2,7 @@ async function newFormHandler(event) {
   event.preventDefault();
 
   const title = document.querySelector('input[name="post-title"]').value;
-  const blog_content = document.querySelector('input[name="post-url"]').value;
+  const blog_content = document.querySelector('#post-content').value;
 
   const response = await fetch(`/api/posts`, {
     method: 'POST',
